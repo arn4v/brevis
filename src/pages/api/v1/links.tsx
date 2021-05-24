bc80c22890a378bb36c576af2dc367244bb4e78b
Arnav Gosain <arnav@arnavgosain.com>
2021-05-24 23:11:40 +0530

    feat(api): check if link id already exists and return existing id

--- src/pages/api/v1/links.tsx
+++ src/pages/api/v1/links.tsx
@@ -7,23 +7,28 @@ import { BASE_URL } from "../../../lib/constants";
 
 export default nextConnect<NextApiRequest, NextApiResponse>().post(
   async (req, res) => {
-    if (typeof req.body === "string")
-      return res
-        .status(422)
-        .send({ message: "Request must contain body of type string (URL)." });
-    const link = Object.keys(req.body);
-    if (link.length > 0) {
-      if (isValidHttpUrl(link[0])) {
-        const id = nanoid();
-        redis.set(id, link[0]);
-        res.status(200).send(BASE_URL + "/" + id);
+    const contentType = req.headers["content-type"];
+    if (contentType === "text/plain") {
+      const url = req.body;
+      if (isValidHttpUrl(url)) {
+        const existingId = await redis.get(url);
+        if (typeof existingId === "string") {
+          res.status(409).send(BASE_URL + "/" + existingId);
+        } else {
+          const id = nanoid();
+          await redis.set(id, url);
+          await redis.set(url, id);
+          res.status(200).send(BASE_URL + "/" + id);
+        }
       } else {
         res
           .status(400)
           .send({ message: "Body string is not a valid http url." });
       }
     } else {
-      res.status(422).send({ message: "Body must be the url string." });
+      res
+        .status(400)
+        .send({ message: 'Content-Type must always be "text/plain".' });
     }
   }
 );
